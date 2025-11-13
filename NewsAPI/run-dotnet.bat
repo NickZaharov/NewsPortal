2dbd95e7f238fa75cb511bf1cea077ceb9c4f338
NickZaharov <63459696+NickZaharov@users.noreply.github.com>
2025-11-13 19:13:49 +0300

    Fixed launch script

--- NewsAPI/run-dotnet.bat
+++ NewsAPI/run-dotnet.bat
@@ -4,7 +4,8 @@ chcp 65001
 setlocal
 
 set PROJECT=./NewsAPI/NewsAPI.csproj
-
 set UseRedis=false
+
+dotnet build %PROJECT%
 
 dotnet run --project %PROJECT%
--- run-all.bat
+++ run-all.bat
@@ -1,9 +1,6 @@
 @echo off
 setlocal
-chcp 65001
 
-echo Запуск .NET-приложения...
 start "" ".\NewsApi\run-dotnet.bat"
 
-echo Запуск Vue-приложения...
 start "" ".\NewsSPA\run-vue.bat"
